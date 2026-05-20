e0a9dac0343e40d0b7b0180a43c3a2e4e0cd4b74
ahmed elbardawell <brdweelahmed@gmail.com>
2026-05-20 14:36:39 +0300

    Force HTTPS scheme in production and trust all load balancer proxies

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (env('APP_ENV') === 'production' || (isset($_SERVER['HTTP_X_FORWARDED_PROTO']) && $_SERVER['HTTP_X_FORWARDED_PROTO'] === 'https')) {
+            \URL::forceScheme('https');
+        }
+
         Vite::prefetch(concurrency: 3);
     }
 }
