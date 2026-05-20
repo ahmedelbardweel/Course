9d0278c4a1ec96ca52c06e243cd5e4874caf5bb5
ahmed elbardawell <brdweelahmed@gmail.com>
2026-05-20 14:30:03 +0300

    Expose Vite manifest.json from hidden directory and fix Lucide import in Edit.vue

--- resources/js/Pages/Profile/Edit.vue
+++ resources/js/Pages/Profile/Edit.vue
@@ -4,7 +4,7 @@ import DeleteUserForm from './Partials/DeleteUserForm.vue';
 import UpdatePasswordForm from './Partials/UpdatePasswordForm.vue';
 import UpdateProfileInformationForm from './Partials/UpdateProfileInformationForm.vue';
 import { Head } from '@inertiajs/vue3';
-import { ChevronLeft, User, ShieldLock, UserX } from 'lucide-vue-next';
+import { ChevronLeft, User, Shield, UserX } from 'lucide-vue-next';
 
 defineProps({
     mustVerifyEmail: {
